f5555caf40210f7b736ac5d2bb31b6ff602a53d1
Aleksandr Orlov <aleksandr.orlov@sputnix.ru>
2021-05-23 22:46:01 +0300

    Added command_list.h

---
+++ command_list.h
@@ -0,0 +1,41 @@
+/*
+ * command_list.h
+ *
+ *  Created on: 23 ��� 2021 �.
+ *      Author: Danila
+ */
+
+#ifndef MAIN_COMMAND_LIST_H_
+#define MAIN_COMMAND_LIST_H_
+
+#include <stdint.h>
+
+//struct command {
+//    uint16_t comman_id;
+//    void* data;
+//}__attribute__((packed));
+
+enum command_list
+{
+	set_throttle_comm,
+	set_orientation_comm,
+	start_comm, // empty
+	stop_comm, // empty
+};
+
+struct set_throttle
+{
+	float value;
+}__attribute__((packed));
+
+struct set_orientation
+{
+	float pitch;
+	float roll;
+	float yaw;
+}__attribute__((packed));
+
+
+
+
+#endif /* MAIN_COMMAND_LIST_H_ */
